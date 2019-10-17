09f260c73ec28e2c9869db077480a9def31fc904
Bishal Ghimire <bishalghimire@Bishals-MacBook-Air.local>
2019-10-17 16:18:05 -0500

    designed button

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity , Alert} from 'react-native';
 
-export default function App() {
+export default class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      uri: require('./src/images/dice1.png')
+    }
+  }
+  
+  playButtonPressed =()=>{
+    Alert.alert('App is working')
+  }
+  
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+      source ={ this.state.uri}
+      />
+      <TouchableOpacity 
+      onPress ={this.playButtonPressed}
+      >
+        <Text style = {styles.buttonStyle}
+        >Play</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E74292',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonStyle:{
+    marginTop: 35,
+    fontSize: 20,
+    color: '#FFF',
+    fontWeight: "bold",
+    borderWidth: 2,
+    paddingVertical: 8,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    borderColor: '#FFF'
+
+  }
 });
